3b69c90bc231758d6de0093bee471b010a9cbe44
Logan McNulty <logan.mcnulty@zmfs.local>
2019-08-24 08:34:35 -0400

    final commit for HW submission

--- Psychic Game Pseudo-Code.md
+++ Psychic Game Pseudo-Code.md
@@ -1,3 +1,11 @@
-1. Objective of game: User has 9 guesses to guess which letter of the alphabet the psychic is thinking of
-2. Create Skeleton using HTML and CSS: Complete
-3. 
+<!-- - Objective of game: User has 10 guesses to guess which letter of the alphabet the psychic is thinking of -->
+<!-- - Create Skeleton using HTML and CSS -->
+<!-- - Prompt the user via an alert to guess using their keyboard -->
+<!-- - create an array of all letters in the alphabet -->
+<!-- - document.onkeyup function to begin the game -->
+<!-- - use a random math function to choose a letter -->
+<!-- - user enters a key for their guess on the keyboard -->
+<!-- - the guess checks for a match to the stored letter -->
+<!-- - if not a match, deduct from guesses remaining -->
+<!-- - if a match, add to wins, and reset the game -->
+<!-- - if no match after 10 guesses, add to losses, and reset the game -->
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,70 @@
 
 // global variables defined
-
 var alphabet = ["a", "b", "c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var userWins = 0;
 var userLosses = 0;
-var guessesRemaining = 9;
-var guessesSoFar = " ";
+var guessesRemaining = 10;
+var guessesSoFar = [];
+
+// Eqution for picking secret letter 
+var secretLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
 
 //variables for replacing text in HTML
-
 var userWinsText = document.getElementById("wins");
 var userLossesText = document.getElementById("losses");
 var guessesRemainingText = document.getElementById("guessesRemaining");
 var guessesSoFarText = document.getElementById("guessesSoFar");
 
-// alert("use your keyboard to guess what letter i'm thinking of")
+alert("use your keyboard to guess what letter i'm thinking of")
+
+//peak at the log if you want to cheat! 
+console.log(secretLetter);
 
 // game begins
-
 document.onkeyup = function(event) {
-    var newGuessesRemaining = guessesRemaining--;
+    //store user choice to key selected, and console to log to confirm
     var userGuess = event.key;
-    var secretLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-    console.log(newGuessesRemaining);
+    console.log(secretLetter);
 
-    // A loop that says, stop making these changes once guesses remaining reaches 0
-        // while (newGuessesRemaining > 0) {
-            if (userGuess === secretLetter) {
-                userWins++;
-            }
-                else {
-                    // userLosses++; <-- I know that this is wrong because it shouldn't be updated until guesses remaining reaches 0.
-                    var newGuessText = document.createElement("p");
-                }
-            guessesRemainingText.textContent = guessesRemaining;
-            userWinsText.textContent = userWins;
-            userLossesText.textContent = userLosses;
-            newGuessText.textContent=(" ," + userGuess);
-            console.log(newGuessText);
-            guessesSoFarText.appendChild(newGuessText);
-// }
-}
+//Only execute the following if the user's guess = secret letter
+    if (userGuess === secretLetter){
+        //add 1 to user wins
+        userWins++;
+        //store new user wins to "userWinsText"
+        userWinsText.textContent = userWins;
+        // update guessees remaining to 10 (so the variable knows), and replace starting text with 10 
+        //(there is a difference between the two, hence the two lines)
+        guessesRemaining = 10;
+        guessesRemainingText.textContent = 10;
+        // clear the guessed letters variable, and store to text
+        guessesSoFar=[];
+        guessesSoFarText.textContent = guessesSoFar;
+        //update the secret letter, log to console
+        secretLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+    }
+    //otherwise (if user guess does not = computer guess)
+    else{
+        //reduce guesses remaining by 1, and store to text
+        guessesRemaining--;
+        guessesRemainingText.textContent = guessesRemaining;
+        //add to guessed letters, we use push in order to add to an array
+        guessesSoFar.push(userGuess);
+        guessesSoFarText.textContent = guessesSoFar;
+
+    }
+    //this line is for adding to losses and resetting the game when guessesRemaining = 0
+    if(guessesRemaining === 0){
+        //add to losses by 1, and store to text
+        userLosses++;
+        userLossesText.textContent = userLosses;
+        //update guesses remaining to 10 and store to text
+        guessesRemaining = 10;
+        guessesRemainingText.textContent = 10;
+        //update the secret letter, log to console
+        secretLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+        console.log(secretLetter);
+        //clear the guesses so far array, and store to text
+        guessesSoFar=[];
+        guessesSoFarText.textContent = guessesSoFar;
+    }
 }
